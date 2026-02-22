3e15ee689719b820a110c8620b49bd2c945a26d4
Alberto Parot <albertoparot@MacBook-Pro-de-Alberto.local>
2026-02-22 13:17:22 -0300

    Fix Expo asset rewrites

--- admin/next.config.js
+++ admin/next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     return [
       { source: "/", destination: "/portal/index.html" },
       { source: "/assets/:path*", destination: "/portal/assets/:path*" },
+      { source: "/_expo/:path*", destination: "/portal/_expo/:path*" },
       { source: "/favicon.ico", destination: "/portal/favicon.ico" },
       { source: "/manifest.json", destination: "/portal/manifest.json" },
       { source: "/expo-service-worker.js", destination: "/portal/expo-service-worker.js" },
